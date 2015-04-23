cef3c4f7e5f1c5deceae3abc82d34b508e9ef884
Renato Mendes <renatodz86@gmail.com>
2015-04-23 18:12:26 -0300

    Initial Commit

---
+++ MeetStudent/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MeetStudent
+//
+//  Created by Renato Mendes on 23/04/15.
+//  Copyright (c) 2015 RR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MeetStudent/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MeetStudent
+//
+//  Created by Renato Mendes on 23/04/15.
+//  Copyright (c) 2015 RR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

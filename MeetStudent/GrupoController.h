121d1dcec03820c128006ccfa36f30618e0e7936
Renato Mendes <renatodz86@gmail.com>
2015-04-24 16:59:38 -0300

    Página de criar novo grupo e melhoria de navegação entre as páginas

---
+++ MeetStudent/GrupoController.h
@@ -0,0 +1,15 @@
+//
+//  GrupoController.h
+//  MeetStudent
+//
+//  Created by Renato Mendes on 24/04/15.
+//  Copyright (c) 2015 RR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GrupoController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *nomeNovoGrupo;
+
+@end

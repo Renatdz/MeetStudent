5d7e79f78fcb67a1e415d5557ab481f6677a6d21
Renato Mendes <renatodz86@gmail.com>
2015-04-23 21:14:44 -0300

    Animação storyboard

--- MeetStudent/AppDelegate.m
+++ MeetStudent/AppDelegate.m
@@ -8,7 +8,7 @@
 
 #import "AppDelegate.h"
 
-@interface AppDelegate ()
+@interface AppDelegate () //<UINavigationControllerDelegate>
 
 @end
 
@@ -17,7 +17,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-
+//    [(UINavigationController *) self.window.rootViewController setDelegate:self];
+    
     return YES;
 }
 

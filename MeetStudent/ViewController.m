6eab65af005000ec5fbaab728ade3faa7044d24b
rafael.cabral <rcabral24ward.com.br>
2015-05-07 19:10:17 -0300

    Mensagem de confirmação do usuário em relação à política de privacidade.

--- MeetStudent/ViewController.m
+++ MeetStudent/ViewController.m
@@ -10,6 +10,8 @@
 #import "InstituicoesController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *btnCadastro;
+@property (weak, nonatomic) IBOutlet UIButton *politicaDePrivacidade;
 
 @end
 
@@ -24,11 +26,38 @@
     // Dispose of any resources that can be recreated.
 }
 
+//|---------------------------------------
+- (IBAction)politicaPrivacidade:(id)sender {
+    //redirect view politica de privacidade
+    [self performSegueWithIdentifier:@"PoliticaDePrivacidade" sender:self];
+}
+
 //|-------------------------------------------------
 //|return for view controller
 - (IBAction)returnMainViewController:(UIStoryboardSegue*)sender
 {
 
+}
+- (IBAction)cadastrar:(id)sender {
+    
+    //popup confirm
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Atenção!"
+                                                    message:@"Ao clicar em `Concordar` você concorda com nossa Política de Privacidade!\nCaso ainda não tenha lido, por favor leia."
+                                                   delegate:self
+                                          cancelButtonTitle:@"Ler"
+                                          otherButtonTitles:@"Concordar",nil];
+    [alert show];
+}
+//|-----------------------------------------------
+- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex {
+    if (buttonIndex == 1) {
+        //redirect view cadastro
+        [self performSegueWithIdentifier:@"Cadastrar" sender:self];
+    }else{
+        //redirect view politica de privacidade
+        [self performSegueWithIdentifier:@"PoliticaDePrivacidade" sender:self];
+    }
+    
 }
 
 
